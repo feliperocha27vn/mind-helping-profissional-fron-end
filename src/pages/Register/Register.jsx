b395ac68f4bb42fb6d4b73f8ab6b54145216d232
Felipe <feliperocha.dev27@gmail.com>
2025-04-18 21:05:17 -0300

    feat: finalizando responsidade tela de registro [

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -42,120 +42,136 @@ export function Register() {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center">
-      <form className="w-full flex flex-col items-center p-6">
-        <h1 className="text-3xl w-7/12 text-center">
-          Cadastro de Profissional
-        </h1>
-        <div className="mt-10 w-full space-y-5">
-          <div className="space-y-1">
-            <p className="font-light">Nome</p>
-            <Input type="text" name="nome" />
-          </div>
-          <div className="flex items-center gap-x-3">
-            <div>
-              <p>Data de Nascimento</p>
-              <Input type="text" name="data_nascimento" />
+    <div className="hidden lg:min-h-screen lg:bg-blue-500 lg:flex lg:items-center lg:justify-between">
+      <div className="flex flex-col items-center justify-center lg:h-full lg:bg-white lg:px-8 lg:w-8/12">
+        <form className="w-full flex flex-col items-center p-6 max-w-2xl">
+          <h1 className="text-3xl w-7/12 text-center lg:w-10/12">
+            Cadastro de Profissional
+          </h1>
+          <div className="mt-10 w-full space-y-5">
+            <div className="space-y-1 w-full">
+              <p className="font-light">Nome</p>
+              <Input type="text" name="nome" />
             </div>
-            <div className="self-end">
-              <p>CPF</p>
-              <Input type="text" name="cpf" />
+            <div className="flex items-center gap-x-3 w-full">
+              <div className="w-6/12">
+                <p>Data de Nascimento</p>
+                <Input type="text" name="data_nascimento" />
+              </div>
+              <div className="self-end w-6/12">
+                <p>CPF</p>
+                <Input type="text" name="cpf" />
+              </div>
             </div>
-          </div>
-          <div className="flex items-center gap-x-3">
-            <div>
-              <p>Telefone</p>
-              <Input type="text" name="telefone" />
+            <div className="flex items-center gap-x-3 w-full">
+              <div className="w-6/12">
+                <p>Telefone</p>
+                <Input type="text" name="telefone" />
+              </div>
+              <div className="self-end w-6/12">
+                <p>CRP</p>
+                <Input type="text" name="crp" />
+              </div>
             </div>
-            <div className="self-end">
-              <p>CRP</p>
-              <Input type="text" name="crp" />
+            <div className="space-y-1">
+              <p className="font-light">E-mail</p>
+              <Input type="text" name="email" />
             </div>
-          </div>
-          <div className="space-y-1">
-            <p className="font-light">E-mail</p>
-            <Input type="text" name="email" />
-          </div>
-          <div className="flex items-center gap-x-3">
-            <div>
-              <p>CEP</p>
+            <div className="flex items-center gap-x-3">
+              <div className="w-6/12">
+                <p>CEP</p>
+                <Input
+                  type="text"
+                  name="cep"
+                  onHandlerBlur={handleChangeBlurCep}
+                />
+              </div>
+              <div className="self-end w-6/12">
+                <p>Número</p>
+                <Input type="text" name="numero" />
+              </div>
+            </div>
+            <div className="space-y-1">
+              <p className="font-light">Logradouro</p>
               <Input
                 type="text"
-                name="cep"
-                onHandlerBlur={handleChangeBlurCep}
-              />
-            </div>
-            <div className="self-end">
-              <p>Número</p>
-              <Input type="text" name="numero" />
-            </div>
-          </div>
-          <div className="space-y-1">
-            <p className="font-light">Logradouro</p>
-            <Input
-              type="text"
-              name="logradouro"
-              inputValue={dataCep.logradouro}
-              onHandlerChange={handleChange}
-            />
-          </div>
-          <div className="flex items-center gap-x-3">
-            <div>
-              <p>Bairro</p>
-              <Input
-                type="text"
-                name="bairro"
-                inputValue={dataCep.bairro}
+                name="logradouro"
+                inputValue={dataCep.logradouro}
                 onHandlerChange={handleChange}
               />
             </div>
-            <div className="self-end">
-              <p>Cidade</p>
+            <div className="flex items-center gap-x-3">
+              <div className="w-6/12">
+                <p>Bairro</p>
+                <Input
+                  type="text"
+                  name="bairro"
+                  inputValue={dataCep.bairro}
+                  onHandlerChange={handleChange}
+                />
+              </div>
+              <div className="self-end w-6/12">
+                <p>Cidade</p>
+                <Input
+                  type="text"
+                  name="cidade"
+                  inputValue={dataCep.localidade}
+                  onHandlerChange={handleChange}
+                />
+              </div>
+            </div>
+            <div className="space-y-1">
+              <p className="font-light">Complemento</p>
               <Input
                 type="text"
-                name="cidade"
-                inputValue={dataCep.localidade}
+                name="complemento"
                 onHandlerChange={handleChange}
               />
             </div>
-          </div>
-          <div className="space-y-1">
-            <p className="font-light">Senha</p>
-            <Input type="text" name="senha" />
-          </div>
-          <div className="space-y-1">
-            <p className="font-light">Repita sua senha</p>
-            <Input type="text" name="" />
-          </div>
-          <div className="items-top flex space-x-2">
-            <Checkbox id="terms1" className="border-zinc-500" />
-            <div className="grid gap-1.5 leading-none">
-              <label
-                htmlFor="terms1"
-                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-              >
-                Profissional social ?
-              </label>
-              <p className="text-sm text-muted-foreground">
-                (Profissional social é aquele que oferece consultas de forma
-                gratuita para pessoas de baixa renda)
-              </p>
+            <div className="space-y-1">
+              <p className="font-light">Senha</p>
+              <Input type="text" name="senha" />
+            </div>
+            <div className="space-y-1">
+              <p className="font-light">Repita sua senha</p>
+              <Input type="text" name="" />
+            </div>
+            <div className="items-top flex space-x-2">
+              <Checkbox id="terms1" className="border-zinc-500" />
+              <div className="grid gap-1.5 leading-none">
+                <label
+                  htmlFor="terms1"
+                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                >
+                  Profissional social ?
+                </label>
+                <p className="text-sm text-muted-foreground">
+                  (Profissional social é aquele que oferece consultas de forma
+                  gratuita para pessoas de baixa renda)
+                </p>
+              </div>
+            </div>
+            <div className="items-top flex space-x-2">
+              <Checkbox id="terms2" className="border-zinc-500" />
+              <div className="grid gap-1.5 leading-none">
+                <label
+                  htmlFor="terms2"
+                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                >
+                  Li e aceito os termos de condição
+                </label>
+              </div>
             </div>
           </div>
-          <div className="items-top flex space-x-2">
-            <Checkbox id="terms2" className="border-zinc-500" />
-            <div className="grid gap-1.5 leading-none">
-              <label
-                htmlFor="terms2"
-                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-              >
-                Li e aceito os termos de condição
-              </label>
-            </div>
-          </div>
+          <Button className={'mt-10 py-6 px-5 bg-blue-400'}>Cadastrar</Button>
+        </form>
+      </div>
+      <div className="hidden lg:block lg:pr-8">
+        <div className="bg-blue-500 text-white w-full">
+          <h1 className="title-mindHelping text-5xl">MIND HELPING</h1>
+          <p className="text-end">Profissional</p>
         </div>
-        <Button className={'mt-10 py-6 px-5 bg-blue-400'}>Cadastrar</Button>
-      </form>
+      </div>
     </div>
   )
 }
