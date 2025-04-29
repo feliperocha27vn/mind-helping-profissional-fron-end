9045dc9b912f92157b7c5f17a59cd2254a1f748f
Felipe <feliperocha.dev27@gmail.com>
2025-04-29 20:18:25 -0300

    chore: adicionando page register na pasta Register

---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,10 @@
+// Components
+import { RegisterForm } from './RegisterForm'
+
+export function Register() {
+  return (
+    <div className="min-h-screen w-full flex justify-center items-center bg-slate-100 lg:bg-slate-400 lg:p-5">
+      <RegisterForm />
+    </div>
+  )
+}
